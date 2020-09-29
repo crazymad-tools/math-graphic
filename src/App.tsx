d09562c14d9d311fc43f7d800f428cf89851d410
crazymad <crazymad0601@gmail.com>
2020-09-29 22:24:27 +0800

    init project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
